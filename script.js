4c233e2f522eaa8c8a6d0b48fe6160afd28982d3
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-19 10:06:54 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
+	var radius=document.getElementById("radius");
+	var r=parseFloat(radius);
+	if(isNaN(r)||r<0)
+	{
+		document.getElementById("volume").value='NaN';
+	}
+	else{
+		var volume = (4/3) * Math.PI * Math.pow(r, 3);
+		volume = volume.toFixed(4);
+		document.getElementById("volume").value=valume
+	}
   
 } 
 
